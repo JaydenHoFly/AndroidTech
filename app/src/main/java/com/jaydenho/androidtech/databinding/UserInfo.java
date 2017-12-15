8b85424c87fd43758c393e5d325c24ea91218b3b
hedazhao <hedazhao@xunlei.com>
2017-12-15 17:11:02 +0800

    [DataBinding]学习Bindable注解

--- app/src/main/java/com/jaydenho/androidtech/databinding/UserInfo.java
+++ app/src/main/java/com/jaydenho/androidtech/databinding/UserInfo.java
@@ -3,15 +3,20 @@ package com.jaydenho.androidtech.databinding;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.ObservableField;
+
 import com.jaydenho.androidtech.BR;
 
 /**
  * Created by hedazhao on 2016/7/22.
+ * 必须要实现getter,setter方法。
+ * 实现双向绑定：
+ * 1、使用ObserableField对象；
+ * 2、实现BaseObserable接口，在属性的getter方法中使用@Bindable注解，会在BR中生成对应的id，然后在setter方法中调用notifyPropertyChanged方法通知xml, 对象UserInfo中的某个字段发生了改变。
  */
 public class UserInfo extends BaseObservable {
     public final ObservableField<Float> score = new ObservableField<>();
-    private  String name;
-    private  int age;
+    private String name;
+    private int age;
 
     public UserInfo(String name, int age) {
         this.name = name;
