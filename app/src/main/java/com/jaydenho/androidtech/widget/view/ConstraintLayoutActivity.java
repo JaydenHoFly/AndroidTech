0cae46648dc1489506ae8ae197207fe758993734
hedazhao <hedazhao@xunlei.com>
2018-11-05 18:06:06 +0800

    constraintlayout

--- app/src/main/java/com/jaydenho/androidtech/widget/view/ConstraintLayoutActivity.java
+++ app/src/main/java/com/jaydenho/androidtech/widget/view/ConstraintLayoutActivity.java
@@ -21,31 +21,5 @@ public class ConstraintLayoutActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constraint_layout_01);
-        final ConstraintLayout rootView = findViewById(R.id.root_view);
-        final ImageView iv4 = findViewById(R.id.imageView4);
-        final ConstraintSet constraintSet1 = new ConstraintSet();
-        constraintSet1.clone(this, R.layout.constraint_layout_01);
-        iv4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    TransitionManager.beginDelayedTransition(rootView);
-                }
-                if (flag = !flag) {
-                    constraintSet1.constrainWidth(R.id.imageView4, ConstraintSet.MATCH_CONSTRAINT);
-                    constraintSet1.constrainHeight(R.id.imageView4, 400);
-                    constraintSet1.connect(R.id.imageView4, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
-                    constraintSet1.applyTo(rootView);
-                } else {
-                    constraintSet1.constrainWidth(R.id.imageView4, 500);
-                    constraintSet1.constrainHeight(R.id.imageView4, 200);
-                    constraintSet1.connect(R.id.imageView4, ConstraintSet.TOP, R.id.iv_b1, ConstraintSet.BOTTOM);
-                    constraintSet1.applyTo(rootView);
-                }
-                rootView.invalidate();
-                rootView.requestLayout();
-            }
-        });
     }
 }
